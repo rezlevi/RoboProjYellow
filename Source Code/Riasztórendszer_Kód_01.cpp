b9514bfd8a21e8ad8a1ad817f83ffd9d44157b20
StangaPeter <113850216+StangaPeter@users.noreply.github.com>
2023-06-14 17:32:40 +0200

    DONE!

--- Source Code/Riasztórendszer_Kód_01.cpp
+++ Source Code/Riasztórendszer_Kód_01.cpp
@@ -1,277 +1,394 @@
 #include <Servo.h>
+#include <SPI.h>
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-#include <Keypad.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#include <IRremote.h>
+#include <MFRC522.h>
 
-#define ledGreen 10
-#define ledRed 11
-#define servo 12
+#define ledGreen 5
+#define ledRed 6
+#define servo 8
 #define pir A0
-#define speaker A1
+#define speaker 3
 
-//Keypadhoz
-char keymap[4][4] =
-{
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
+#define SS_PIN 10
+#define RST_PIN 9
 
-byte rowPins[4] = {2, 3, 4, 5};
-byte colPins[4] = {6, 7, 8, 9};
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET 4
 
-Keypad myKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, 4, 4);
+Adafruit_SSD1306 display(OLED_RESET);
+MFRC522 rfid(SS_PIN, RST_PIN);
 
 Servo myservo;
-LiquidCrystal_I2C lcd(32, 16, 2);
-int pirValue = 0; // Riasztóhoz
+int pirValue = 0; //Riasztóhoz
 int previousMillis = 0;
-int interval = 150;
+int interval = 500;
 int state = 0;
 char key;
 String code = "";
-int ledState = LOW;
 
-int offState() //Kikapcsolt állapot
-{
-  digitalWrite(ledGreen, LOW);
-  lcd.noBacklight();
-  lcd.clear();
-
-  while (true)
-  {
-    key = myKeypad.getKey();
-    if (key == 'D')
-    {
-      state = 1;
-      return state;
-    }
-  }
-  return 0;
-}
-
-int onState() //Bekapcsolt, nem készenléti állapot
-{
-  initialiseLCD();
-  myservo.write(180);
-  lcd.backlight();
-  lcd.print("Security code:");
-  lcd.setCursor(0, 1);
-  int wrongCodeCounter = 0;
-  
-  while (true)
-  {
-    key = myKeypad.getKey();
-    switch (key)
-    {
-      case 'D':
-      	state = 0;
-      	return state;
-      	break;
-      case 'C':
-      	initialiseLCD();
-      	break;
-      case 'A':
-      	if (code == "911")
-        {
-          state = 2;
-          return state;
-          break;
-        }
-        else
-        {
-          handleInvalidCode();
-          wrongCodeCounter++;
-          break;
-        }
-      default:
-        if (code.length() < 3 && key != NO_KEY)
-        {
-          code += key;
-          lcd.print(key);
-        }
-        if (wrongCodeCounter >= 3)
-        {
-          state = 4;
-          return state;
-          break;
-        }
-        break;
-    }
-  }
-  return 0;
-}
-
-int standbyState() //Bekapcsolt, készenléti állapot
-{
-  initialiseLCD();
-  lcd.print("Ready to serve");
-  myservo.write(0);
-
-  while (true)
-  {
-    key = myKeypad.getKey();
-    checkKey(key);
-    pirValue = analogRead(pir);
-    if (pirValue > 100)
-    {
-      state = 3;
-      return state;
-    }
-  }
-  return 1;
-}
-
-int alarmState() //Riasztási állapot
-{
-  initialiseLCD();
-  lcd.print("INTRUDER HERE");
-
-  while (true)
-  {
-    //LED + Hangszóró
-    unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= interval)
-    {
-      previousMillis = currentMillis;
-      tone(speaker, 500, 100);
-      delay(300);
-      tone(speaker, 440, 150);
-      delay(300);
-      alarmLights();
-      digitalWrite(ledRed, ledState);
-    }
-
-    key = myKeypad.getKey();
-    checkKey(key);
-    if (state == 4)
-    {
-      return state;
-    }
-  }
-  return 1;
-}
-
-void lockDownState() // Lezárt riasztási állapot
-{
-  initialiseLCD();
-  lcd.print("CLOSED");
-
-  while (true)
-  {
-    //LED + Hangszóró
-    unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= interval)
-    {
-      previousMillis = currentMillis;
-      tone(speaker, 440, 150);
-      alarmLights();
-      digitalWrite(ledRed, ledState);
-    }
-  }
-}
-
-void alarmLights()
-{
-  if (ledState == LOW)
-  {
-    ledState = HIGH;
-  }
-  else
-  {
-    ledState = LOW;
-  }
-}
-
-void initialiseLCD()
-{
-  lcd.clear();
-  code = "";
-  digitalWrite(ledGreen, LOW);
-  digitalWrite(ledRed, HIGH);
-}
-
-void checkKey(char key)
-{
-  int wrongCodeCounter = 0;
-  switch (key)
-  {
-    case 'D':
-      state = 1;
-      break;
-    case 'C':
-      initialiseLCD();
-      break;
-    case 'A':
-      if (code == "911")
-      {
-        state = 1;
-      }
-      else
-      {
-        handleInvalidCode();
-        wrongCodeCounter++;
-      }
-      break;
-    default:
-      if (code.length() < 3 && key != NO_KEY && key != 'D')
-      {
-        code += key;
-        lcd.print(key);
-      }
-      break;
-  }
-  if (wrongCodeCounter >= 3)
-  {
-    state = 4;
-  }
-}
-
-void handleInvalidCode()
-{
-  code = "";
-  lcd.clear();
-  lcd.print("Invalid code!");
-  delay(2000);
-  lcd.clear();
-  lcd.print("Try again!");
-  delay(2000);
-  lcd.clear();
-}
+#define IR_RECEIVE_PIN 2
+decode_results results;
 
 void setup()
 {
   Serial.begin(9600);
+  SPI.begin(); // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522    
   pinMode(ledGreen, OUTPUT);
   pinMode(ledRed, OUTPUT);
-  pinMode(pir, INPUT);
-  pinMode(speaker, OUTPUT);
+  pinMode(pir,INPUT);
+  pinMode(speaker,OUTPUT);
   myservo.attach(servo);
-  lcd.begin();
+  myservo.write(180);
+  IrReceiver.begin(IR_RECEIVE_PIN);
+  
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.display();
+
 }
+void(* resetFunc)(void)=0;
+
+char getKey()
+{
+  char key;
+  if (IrReceiver.decode())
+      {
+      switch(IrReceiver.decodedIRData.command)
+        {
+          case 69: key = '1';
+           break;
+          case 70: key =  '2';
+           break;
+          case 71: key =  '3';
+           break;
+          case 68: key =  '4';
+           break;
+          case 64: key =  '5';
+           break;
+          case 67: key =  '6';
+           break;
+          case 7: key =  '7';
+           break;
+          case 21: key =  '8';
+           break;
+          case 9: key =  '9';
+           break;
+          case 25: key =  '0';
+           break;
+          case 22: key =  'D';
+           break;
+          case 13: key =  'C';
+           break;
+          case 28: key =  'A';
+           break;
+          default:
+            break;
+        }
+        IrReceiver.resume();
+        return key;
+      }
+  key = 'F';
+  return key;
+}
+
 
 void loop()
 {
-  switch (state)
+  
+  //Kikapcsolt állapot
+  if(state == 0)
   {
-    case 0:
-	offState();
+    digitalWrite(ledGreen, LOW);
+    digitalWrite(ledRed, LOW);
+
+    display.clearDisplay();
+    display.display();
+
+    while(true)
+    {
+      key = getKey();
+      if(key == 'D'){state = 1; break;}
+    }
+  }
+  
+  //Bekapcsolt, nem készenléti állapot
+  if(state == 1)
+  {
+   code = "";
+   digitalWrite(ledGreen, HIGH);
+   digitalWrite(ledRed, LOW);
+
+   display.clearDisplay();
+   display.display();
+ 
+    
+    while(true)
+    {
+      
+      key = getKey();
+      if(key == 'D')
+      {state = 0;
+       break;}
+      else if(key == 'C')
+      {
+        code = "";
+    
+        display.clearDisplay();
+        display.display();
+    
+      }
+      else if(key == 'A')
+      {
+        if(code == "911")
+        {
+          state = 2;
+          break;
+        }
+        else
+        {
+          code = "";
+      
+          display.clearDisplay();
+          display.display();
+      
+          display.setCursor(0,0); 
+          display.println("Invalid code");
+          display.display();
+          delay(2000);
+      
+          display.clearDisplay();
+          display.display();
+      
+          display.setCursor(0,0); 
+          display.println("Try again!");
+          display.display();
+          delay(2000);
+      
+          display.clearDisplay();
+          display.display();
+      
+        }    
+      }
+      else if(code.length() < 3 && key != 'F')
+      {
+        code = code + key;
+    
+        display.clearDisplay();
+        display.display();
+    
+        display.setCursor(0,0); 
+        display.println(code);
+        display.display();
+      }
+      
+    }
+    
+  }
+  
+  //Bekapcsolt, készenléti állapot
+  if(state == 2)
+  {
+    code = "";
+    digitalWrite(ledGreen, LOW);
+    digitalWrite(ledRed, HIGH);
+
+    display.clearDisplay();
+    display.display();
+ 
+    int value = analogRead(pir);
+    
+    while(true)
+    {
+      key = getKey();
+      if(key == 'C')
+      {
+        code = "";
+    
+        display.clearDisplay();
+        display.display();
+    
+      }
+      else if(key == 'A')
+      {
+        if(code == "911")
+        {
+          state = 1;
+          break;
+        }
+        else
+        {
+          code="";
+      
+          display.clearDisplay();
+          display.display();
+      
+          display.setCursor(0,0); 
+          display.println("Invalid code");
+          display.display();
+          delay(2000);
+      
+          display.clearDisplay();
+          display.display();
+      
+          display.setCursor(0,0); 
+          display.println("Try again!");
+          display.display();
+          delay(2000);
+      
+          display.clearDisplay();
+          display.display();
+      
+        }    
+      }
+      else if(code.length() < 3 && key != 'F' && key != 'D')
+      {
+        code = code + key;
+    
+        display.clearDisplay();
+        display.display();
+    
+        display.setCursor(0,0); 
+        display.println(code);
+        display.display();
+      }
+      
+      pirValue = analogRead(pir);
+      if(abs(value - pirValue) > 200)
+      {
+        state = 3;
         break;
-    case 1:
-	onState();
-	break;
-    case 2:
-	standbyState();
-	break;
-    case 3:
-	alarmState();
-	break;
-    case 4:
-	lockDownState();
-	break;
-    default:
-    	lcd.print("Guru meditation!");
-    	break;
+      }
+    }
+  }
+  
+  
+  //Riasztási állapot
+  if(state == 3)
+  {
+    code = "";
+
+    display.clearDisplay();
+    display.display();
+
+    display.setCursor(0,0); 
+    display.print("Alarm!");
+    display.display();
+    int wrongCodeCounter = 0;
+    bool invalid = false;
+    digitalWrite(ledRed, HIGH);
+    delay(200);
+    digitalWrite(ledRed, LOW);
+    delay(200);
+    digitalWrite(ledRed, HIGH);
+    delay(200);
+    digitalWrite(ledRed, LOW);
+    delay(200);
+    digitalWrite(ledRed, HIGH);
+    delay(200);
+    digitalWrite(ledRed, LOW);
+    delay(200);
+    digitalWrite(ledRed, HIGH);
+  unsigned long currentMillis = millis();
+    while(true)
+    {  
+      key = getKey();
+      if (millis() - currentMillis >= 30000)
+      {
+          state = 4;
+          break;
+      }
+      if(key == 'C')
+      {
+        code = "";
+    
+        display.clearDisplay();
+        display.display();
+    
+      }
+      else if(key == 'A')
+      {
+        if(code == "911")
+        {
+          state = 1;
+          break;
+        }
+        else if(wrongCodeCounter < 2)
+        {
+          code="";
+          wrongCodeCounter++;
+      
+          display.clearDisplay();
+          display.display();
+      
+          display.setCursor(0,0); 
+          display.println("Invalid code");
+          display.display();
+          delay(2000);
+      
+          display.clearDisplay();
+          display.display();
+      
+          display.setCursor(0,0); 
+          display.println("Try again!");
+          display.display();
+          delay(2000);
+          display.clearDisplay();
+          display.display();
+          invalid = true;
+        }
+        else
+        {
+          state = 4;
+          break;
+        }
+      }
+      else if(code.length() < 3 && key != 'F'  && key != 'D')
+      {
+        if(invalid == true){Serial.println(" "); invalid = false;}
+        code = code + key;
+    
+        display.clearDisplay();
+        display.display();
+    
+        display.setCursor(0,0); 
+        display.println(code);
+        display.display();
+      }
+    }
+  }
+  
+  
+  //Lezárt riasztási állapot
+  if(state == 4)
+{
+  myservo.write(0);
+  digitalWrite(ledRed, HIGH);
+  display.clearDisplay();
+  display.display(); 
+    display.setCursor(0,0);
+    display.print("Closed!");
+    display.display();
+    
+    while(state == 4)
+    {   
+      tone(speaker,440,150);
+      delay(500);
+      if (rfid.PICC_IsNewCardPresent())
+      {
+          rfid.PICC_ReadCardSerial(); //Megfelelő érték beállításához
+          if(rfid.uid.uidByte[0] == 74)
+          {
+            resetFunc();
+          }
+      }
+    }
   }
 }
